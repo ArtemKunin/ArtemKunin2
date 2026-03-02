3a2d01441bae707869a79cbb261a83ab8c6347d2
Артем Кунин ТКИ-141 <artem.kunin67@gmail.com>
2026-03-02 16:22:32 +0300

    Update tringle.cpp

--- tringle.cpp
+++ tringle.cpp
@@ -13,50 +13,60 @@ double getValue(std::string report = "");
  */
 int main()
 {
-	double x1 = getValue("Введите x1: ");
-	double y1 = getValue("Введите y1: ");
-	double x2 = getValue("Введите x2: ");
-	double y2 = getValue("Введите y2: ");
-	double x3 = getValue("Введите x3: ");
-	double y3 = getValue("Введите y3: ");
-	Triangle myTriangle(x1, y1, x2, y2, x3, y3);
-	std::cout << "Точка H: " << myTriangle.findHeight() << std::endl;
-	std::cout << "Площадь равна " << myTriangle.getPlo() << std::endl;
-	return 0;
+    double x1 = getValue("Введите x1: ");
+    double y1 = getValue("Введите y1: ");
+    double x2 = getValue("Введите x2: ");
+    double y2 = getValue("Введите y2: ");
+    double x3 = getValue("Введите x3: ");
+    double y3 = getValue("Введите y3: ");
+    Triangle myTriangle(x1, y1, x2, y2, x3, y3);
+    std::cout << "Точка H: " << myTriangle.findHeight() << std::endl;
+    std::cout << "Площадь равна " << myTriangle.getPlo() << std::endl;
+    return 0;
 }
 Triangle::Triangle(const double x1, const double y1, const double x2, const double y2, const double x3, const double y3)
 {
-	this->x1 = x1;
-	this->y1 = y1;
-	this->x2 = x2;
-	this->y2 = y2;
-	this->x3 = x3;
-	this->y3 = y3;
+    this->x1 = x1;
+    this->y1 = y1;
+    this->x2 = x2;
+    this->y2 = y2;
+    this->x3 = x3;
+    this->y3 = y3;
+    if (this->getPlo() == 0)
+    {
+        std::cout << "Ошибка! Треугольник не существует!" << std::endl;
+        exit(1);
+    }
+    if ((x3 - x1) * (y2 - y1) - (y3 - y1) * (x2 - x1) == 0)
+	{
+		std::cout << "Ошибка: Треугольник не существует!" << std::endl;
+		exit(1);
+	}
 }
 double Triangle::findHeight() const
 {
-	double BCx = x3 - x2;
-	double BCy = y3 - y2;
-	double BAx = x1 - x2;
-	double BAy = y1 - y2;
-	double t = (BAx * BCx + BAy * BCy) / (BCx * BCx + BCy * BCy);
-	double hx = x2 + t * BCx;
-	double hy = y2 + t * BCy;
-	return sqrt((hx - x1) * (hx - x1) + (hy - y1) * (hy - y1));
+    double BCx = x3 - x2;
+    double BCy = y3 - y2;
+    double BAx = x1 - x2;
+    double BAy = y1 - y2;
+    double t = (BAx * BCx + BAy * BCy) / (BCx * BCx + BCy * BCy);
+    double hx = x2 + t * BCx;
+    double hy = y2 + t * BCy;
+    return sqrt((hx - x1) * (hx - x1) + (hy - y1) * (hy - y1));
 }
 double Triangle::getPlo() const
 {
-	return fabs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2.0;
+    return fabs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2.0;
 }
 double getValue(std::string report)
 {
-	std::cout << report << std::endl;
-	double value = 0;
-	std::cin >> value;
-	if (std::cin.fail())
-	{
-		std::cout << "Ошибка, введено неверное значение!\n";
-		exit(1);
-	}
-	return value;
+    std::cout << report << std::endl;
+    double value = 0;
+    std::cin >> value;
+    if (std::cin.fail())
+    {
+        std::cout << "Ошибка, введено неверное значение!\n";
+        exit(1);
+    }
+    return value;
 }
