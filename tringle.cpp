e4782fcc84a88a78b66dcb0ff27914438a0bd099
Артем Кунин ТКИ-141 <artem.kunin67@gmail.com>
2026-03-03 14:17:14 +0300

    Update tringle.cpp

--- tringle.cpp
+++ tringle.cpp
@@ -1,42 +1,6 @@
 #include <iostream>
 #include <cmath>
 #include "tringle.h"
-Point::Point(double x, double y)
-{
-    this->x = x;
-    this->y = y;
-}
-double Point::getX() const
-{
-    return this->x;
-}
-double Point::getY() const
-{
-    return this->y;
-}
-bool Point::operator==(Point p) const
-{
-    if (this->x == p.x && this->y == p.y)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-bool Point::operator!=(Point p) const
-{
-    if (this->x != p.x || this->y != p.y)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
 Triangle::Triangle(Point p1, Point p2, Point p3)
 {
     this->p1 = p1;
